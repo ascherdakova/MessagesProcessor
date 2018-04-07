74c6b28dafc804cc6bb9d7de2481693d67f9aafd
Vasiliy Matutin <vmatutin@gmail.com>
2018-04-08 02:31:14 +0300

    Prepare app skeleton + Add permission request + Add listening service template

--- app/src/main/java/ru/edu/hse/messagesprocessor/MainActivity.java
+++ app/src/main/java/ru/edu/hse/messagesprocessor/MainActivity.java
@@ -1,10 +1,18 @@
 package ru.edu.hse.messagesprocessor;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.Snackbar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -16,13 +24,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SMS_PERMISSION_CODE = 0;
+
     ArrayList<String> sourceLanguages = new ArrayList<String>();
     ArrayList<String> targetLanguages = new ArrayList<>();
+    CheckBox isEnabled;
+    private View mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLayout = findViewById(R.id.main_layout);
 
         //Translate translate = TranslateOptions.newBuilder().build().getService();
         //com.google.cloud.translate.Translate.LanguageListOption target = Translate.LanguageListOption.targetLanguage("en");
@@ -45,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
         spinnerSourceLanguage.setAdapter(adapter);
         Spinner spinnerTargetLanguage = (Spinner) findViewById(R.id.target_language_spinner);
         spinnerTargetLanguage.setAdapter(adapter);
+
+        //checkbox to check is user want to see service enabled or not
+        isEnabled = (CheckBox) findViewById(R.id.check_box_is_enabled);
+        isEnabled.setOnClickListener(checkBoxOnClickListener);
+        //TODO: save state
 
         // выделяем элемент
         spinnerSourceLanguage.setSelection(1);
@@ -74,5 +93,46 @@ public class MainActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> arg0) {
             }
         });
+    }
+
+    private View.OnClickListener checkBoxOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (isEnabled.isChecked()) {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_SMS)) {
+                        Snackbar.make(mLayout, R.string.sms_access_required, Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, SMS_PERMISSION_CODE);
+                            }
+                        }).show();
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, SMS_PERMISSION_CODE);
+                    }
+                } else {
+                    //TODO separate thread
+                    startService(new Intent(MainActivity.this, ListeningService.class));
+                }
+            } else {
+                stopService(new Intent(MainActivity.this, ListeningService.class));
+            }
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case SMS_PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //TODO separate thread
+                    startService(new Intent(MainActivity.this, ListeningService.class));
+                } else {
+                    isEnabled.setChecked(false);
+                    Snackbar.make(mLayout, R.string.sms_unavailable, Snackbar.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/ru/edu/hse/messagesprocessor/SMSMonitor.java
+++ app/src/main/java/ru/edu/hse/messagesprocessor/SMSMonitor.java
@@ -7,37 +7,23 @@ import android.os.Build;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
 
+import java.util.Objects;
+
 /**
  * Created by Императрица on 01.04.2018.
  */
 
 public class SMSMonitor extends BroadcastReceiver {
-    private static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        /*
-        *
-        * Вариант для обработки для разных версий
-        */
-        /*
-        SmsMessage smsMessage;
-        if (Build.VERSION.SDK_INT >= 19) { //KITKAT
-            SmsMessage[] msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
-            smsMessage = msgs[0];
-        } else {
-            Object pdus[] = (Object[]) bundle.get("pdus");
-            smsMessage = SmsMessage.createFromPdu((byte[]) pdus[0]);
-        }
-        */
 
-        if (intent != null && intent.getAction() != null &&
-                ACTION.compareToIgnoreCase(intent.getAction()) == 0) {
-
-            Object[] pduArray = (Object[]) intent.getExtras().get("pdus");
-            SmsMessage[] messages = new SmsMessage[pduArray.length];
-            for (int i = 0; i < pduArray.length; i++) {
-                messages[i] = SmsMessage.createFromPdu((byte[]) pduArray[i]);
+        if (Objects.equals(intent.getAction(), Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+            StringBuilder smsBody = new StringBuilder();
+            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                smsBody.append(smsMessage.getMessageBody());
             }
+            //TODO: async translation
         }
     }
 }
