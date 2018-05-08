07756f0de8ab6dd70c9b4ae8fb71409ea75c9bd3
Alina <ch.al.sr.1997@gmail.com>
2018-05-08 09:08:35 +0300

    Crash issue fix
    Activity creation moved from AsynTask to Service

--- app/src/main/java/ru/edu/hse/messagesprocessor/SMSTranslator.java
+++ app/src/main/java/ru/edu/hse/messagesprocessor/SMSTranslator.java
@@ -6,12 +6,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.translate.Translation;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class SMSTranslator extends Service {
+    public static final String defaultString = "default";
+
     public SMSTranslator() {
     }
 
@@ -27,7 +32,7 @@ public class SMSTranslator extends Service {
         }
         SharedPreferences sharedPref = this.getApplication().getSharedPreferences(this.getApplication().getString(R.string.custom_shared_preferences), Context.MODE_PRIVATE);
         Boolean isEnabled = sharedPref.getBoolean(this.getApplication().getString(R.string.saved_is_user_enable_service), false);
-        String defaultString = "default";
+
         String targetLanguageCode = sharedPref.getString(this.getApplication().getString(R.string.saved_target_language_code), defaultString);
         String sourceLanguageCode = sharedPref.getString(this.getApplication().getString(R.string.saved_source_language_code), defaultString);
         String sourceText = intent.getStringExtra("sms_body");
@@ -37,10 +42,21 @@ public class SMSTranslator extends Service {
 
         try {
             NetworkTask networkTask =  new NetworkTask(this, sourceLanguageCode, targetLanguageCode, sourceText, GoogleCredentials.fromStream(this.getApplication().getResources().openRawResource(R.raw.credentials)));
-            networkTask.execute();
+            Translation result = networkTask.execute().get();
+
+            //TODO: start the activity through push notification
+            Intent dialogIntent = new Intent(this, TranslationDialog.class).putExtra(TranslationDialog.KEY, result.getTranslatedText());
+            dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(dialogIntent);
+
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
+
         return START_STICKY;
     }
 
