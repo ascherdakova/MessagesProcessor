42a993987e8f8e4c86e8f8aa7c8c5f80fa8f8436
Vasiliy Matutin <vmatutin@gmail.com>
2018-05-10 23:42:01 +0300

    Purpose: Fix bug when notifications don't appear on some devices (Oreo 8.0)

--- app/src/main/java/ru/edu/hse/messagesprocessor/SMSMonitor.java
+++ app/src/main/java/ru/edu/hse/messagesprocessor/SMSMonitor.java
@@ -1,11 +1,13 @@
 package ru.edu.hse.messagesprocessor;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.provider.Telephony;
 import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
@@ -14,11 +16,13 @@ import java.util.Objects;
 public class SMSMonitor extends BroadcastReceiver {
 
     public static final String NOTIFICATION_CHANNEL_ID="message_processor_channel";
+    public static final String NOTIFICATION_CHANNEL_NAME="message_processor_channel_name";
     public static final String KEY_SMS_BODY = "sms_body";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Objects.equals(intent.getAction(), Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+
             NotificationCompat.Builder mBuilder =
                     new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
                             .setSmallIcon(R.drawable.notification_icon)
@@ -40,6 +44,19 @@ public class SMSMonitor extends BroadcastReceiver {
 
             mBuilder.setContentIntent(pendingIntent);
             NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                int importance = NotificationManager.IMPORTANCE_LOW;
+                NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance);
+                notificationChannel.enableLights(true);
+                notificationChannel.setLightColor(Color.RED);
+                notificationChannel.enableVibration(true);
+                notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                if (notificationManager != null) {
+                    notificationManager.createNotificationChannel(notificationChannel);
+                }
+            }
+
             final int notificationId = 1;
             if (notificationManager != null) {
                 notificationManager.notify(notificationId, mBuilder.build());
